63dbd8e8875c5d5156ec8477086e716802677f7a
Nils Steiner <Nils_Steiner@sluz.ch>
2025-03-21 13:13:33 +0100

    Aufgabe 3: multiply()-Funktion umgesetzt und im Testfall Importiert.

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract } from "./calculator";
+import { add, subtract, multiply } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
@@ -17,10 +17,23 @@ test("subtract four from six is two", () => {
   // Arrange
   const a: number = 6;
   const b: number = 4;
-  const expected: number = 2;
+  const expected: number = -2;
 
   // Act
-  const actual: number = subtract(a, b);
+  const actual: number = subtract(b, a);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("multiply two by three is six", () => {
+  // Arrange
+  const a: number = 2;
+  const b: number = 3;
+  const expected: number = 6;
+
+  // Act
+  const actual: number = multiply(a, b);
 
   // Assert
   expect(actual).toBe(expected);
