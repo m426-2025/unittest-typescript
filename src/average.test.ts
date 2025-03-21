f86d58d0977d52e6026c2d4fcdcac930a8021f08
PHIL HÄGELI <phil.haegeli8@gmail.com>
2025-03-21 13:40:30 +0100

    Aufgabe 5 avarage tests + code

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,12 +2,32 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [2, 6];
+  const expected: number = 4;
 
   // Act
   const actual: number = mean(numbers);
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const expected: number = 3;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test mean of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Assert
+  expect(() => mean(numbers)).toThrow("Cannot calculate mean of an empty list");
 });
