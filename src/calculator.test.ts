2e705077cafb9c429f507010d992c6d3bc36d526
julianamschwand <julian_amschwand@sluz.ch>
2025-03-21 15:07:53 +0100

    4.2: Added a test case for division by 0

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -51,3 +51,16 @@ test("divide fifteen by five is three", () => {
   //Assert
   expect(actual).toBe(expected);
 });
+
+test("division by zero is not possible", () => {
+  //Arrange
+  const a: number = 15;
+  const b: number = 0;
+  const expected: number = Infinity;
+
+  //Act 
+  const actual: number = divide(a, b);
+
+  //Assert
+  expect(actual).toBe(expected);
+})
