17c7a872ca83298853b206da441fecd8a693674f
Leandro Schwegler <leandro_schwegler@sluz.ch>
2025-03-21 13:49:03 +0100

    added List with noe elements test and error throw

--- src/average.test.ts
+++ src/average.test.ts
@@ -11,3 +11,23 @@ test("test mean of two numbers", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [2, 4, 6, 8, 10]; // Fünf Zahlen
+  const expected: number = 6; // Arithmetisches Mittel
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("mean of no numbers throws an error", () => {
+  // Arrange
+  const numbers: number[] = []; // Leere Liste
+
+  // Act & Assert
+  expect(() => mean(numbers)).toThrowError("Liste darf nicht leer sein");
+});
