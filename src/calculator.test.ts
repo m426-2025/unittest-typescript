7d4e327d4d9ff50f94d86c3b089a4fc52207daa1
Noah Langensee <noah.langensee@sluz.ch>
2025-03-21 09:12:56 +0100

    Aufgabe 3: Multiplikationsfunktion hinzugefügt und test

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,10 +1,10 @@
-import { add, subtract } from "./calculator";
+import { add, subtract, multiply } from "./calculator";
 
-test("add three to two is five", () => {
+test("add three to five is eight", () => {
   // Arrange
-  const a: number = 2;
+  const a: number = 5;
   const b: number = 3;
-  const expected: number = 5;
+  const expected: number = 8;
 
   // Act
   const actual = add(a, b);
@@ -21,6 +21,19 @@ test("subtract four from six is two", () => {
 
   // Act
   const actual: number = subtract(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("multiply four by three is twelve", () => {
+  // Arrange
+  const a: number = 3;
+  const b: number = 4;
+  const expected: number = 12;
+
+  // Act
+  const actual: number = multiply(a, b);
 
   // Assert
   expect(actual).toBe(expected);
