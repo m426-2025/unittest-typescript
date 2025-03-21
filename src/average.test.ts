e9064eb9a19519e3fd1355c5e1c7b2425da943d0
MARVIN HEGI <MARVIN_HEGI@sluz.ch>
2025-03-21 09:30:28 +0100

    Aufgabe 5 Average test anpassen mit leerer liste und error

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,8 +2,8 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [3, 4];
+  const expected: number = 3.5;
 
   // Act
   const actual: number = mean(numbers);
@@ -11,3 +11,27 @@ test("test mean of two numbers", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [3, 4, 3, 6, 1];
+  const expected: number = 3.4;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test mean of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Act
+
+  // Assert
+  expect(() => mean(numbers)).toThrow();
+});
+
--- src/average.ts
+++ src/average.ts
@@ -1,4 +1,11 @@
 export function mean(numbers: number[]): number {
+  
+  
+  if (numbers.length === 0) {
+    throw new Error("Die leer");
+
+  }
+  
   let sum: number = 0;
   for (let number of numbers) {
     sum += number;
