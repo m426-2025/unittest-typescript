56081c86f7b50701ab50bab37b2d4e5281538363
MARVIN HEGI <MARVIN_HEGI@sluz.ch>
2025-03-21 13:30:45 +0100

    Aufgabe 6 median funktionen und tests hinzufügen

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,4 +1,4 @@
-import { mean } from "./average";
+import { mean , median} from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
@@ -35,3 +35,46 @@ test("test mean of no numbers", () => {
   expect(() => mean(numbers)).toThrow();
 });
 
+
+
+
+
+
+
+
+
+test("test median for odd number of elements", () => {
+  // Arrange
+  const numbers: number[] = [1, 3, 6, 7, 39];
+  const expected: number = 6;
+
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+    expect(actual).toBe(expected);
+
+});
+
+test("test median for even number of elements", () => {
+  // Arrange
+  const numbers: number[] = [1, 7, 5, 6, 4, 9];
+  const expected: number = 5.5;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+    expect(actual).toBe(expected);
+});
+
+test("test median for no elements", () => {
+  // Arrange
+  const numbers: number[] = [];
+// Act
+
+  // Assert
+   expect(() => median(numbers)).toThrow();
+
+});
--- src/average.ts
+++ src/average.ts
@@ -13,3 +13,27 @@ export function mean(numbers: number[]): number {
   const mean: number = sum / numbers.length;
   return mean;
 }
+
+
+
+export function median(numbers: number[]): number {
+  if (numbers.length === 0) {
+    throw new Error("Die Liste ist leer");
+  }
+
+  const sortedNumbers = [...numbers].sort((a, b) => a - b);
+  const length = sortedNumbers.length;
+
+
+  if (length % 2 !== 0) {
+    return sortedNumbers[Math.floor(length / 2)];
+  }
+  else{
+
+    const mid1 = sortedNumbers[length / 2 - 1];
+    const mid2 = sortedNumbers[length / 2];
+    return (mid1 + mid2) / 2;
+  }
+
+}
+
