42c1a0ef75b399e5b1f5a64139f66fe76a87f12b
Erion Jasiqi <erion_jasiqi@sluz.ch>
2025-03-21 15:29:24 +0100

    started aufgabe 6

--- src/average.test.ts
+++ src/average.test.ts
@@ -36,3 +36,37 @@ test("test mean of no numbers", () => {
   expect(actual).toThrow(Error)
 });
 
+test("test median for odd number of elements", () => {
+  // Arrange
+  const numbers: number[] = [3, 8, 10];
+  const expected: number = 6.5;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median for even number of elements", () => {
+  // Arrange
+  const numbers: number[] = [5, 8, 3, 15];
+  const expected: number = 6.5;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median for no elements", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toThrow(Error);
+});
--- src/average.ts
+++ src/average.ts
@@ -11,3 +11,19 @@ export function mean(numbers: number[]): number {
 
   return mean;
 }
+
+export function median(numbers: number[]): number {
+  let sum: number = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  const mean: number = sum / numbers.length;
+
+  if (numbers.length == 0) {
+    throw new Error("No numbers entered")
+  }
+
+  return mean;
+}
+
+
