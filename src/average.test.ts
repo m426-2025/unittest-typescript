e98aec597ef6535fec4753e409202a96b3b11815
Noah Langensee <noah.langensee@sluz.ch>
2025-03-21 10:41:35 +0100

    Aufgabe 6: Mittelwert berechnen hinzugefügt und mehrere Testfälle

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,12 +2,35 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [3, 7];
+  const expected: number = 5;
 
   // Act
   const actual: number = mean(numbers);
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [3,7,2,8,4,6];
+  const expected: number = 5;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test mean of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+  // Act
+
+  // Assert
+  expect(() => {
+      mean(numbers);
+    }).toThrow(new Error("Liste hat keine Elemente"));
 });
