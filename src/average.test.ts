dd50caa4a8b23a0c466d297f1b411ea4a157b530
Eneas Infanger <eneas_infanger@sluz.ch>
2025-03-21 13:25:53 +0100

    Aufgabe 5: Handling von leeren Listen gefixt
    - `mean` funktion in average.ts wirft bei leeren Listen einen Fehler, anstatt `NaN` zurückzugeben

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,12 +2,32 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [13, 6];
+  const expected: number = 9.5;
 
   // Act
   const actual: number = mean(numbers);
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [4, 6, 13, 1, 20];
+  const expected: number = 8.8;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test mean of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Act / Assert
+  expect(() => mean(numbers)).toThrow();
 });
