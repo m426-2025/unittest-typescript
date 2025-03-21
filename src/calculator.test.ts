f8fedd5c50e6ad9749c0b6801c9440e7badf7c22
Artur Ferreira <artur.ferreira@ckw.ch>
2025-03-21 09:03:15 +0100

    Aufgabe 1 - 4.2: implementet 2 new function and created unit tests, that run without errors

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract } from "./calculator";
+import { add, subtract, multiply, divide } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
@@ -21,6 +21,43 @@ test("subtract four from six is two", () => {
 
   // Act
   const actual: number = subtract(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("Multiply 3 by 4 is 12", () => {
+  // Arrange
+  const a: number = 3;
+  const b: number = 4;
+  const expected: number = 12;
+
+  // Act
+  const actual: number = multiply(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+test("divide 15 by 3 is 5", () => {
+  // Arrange
+  const a: number = 15;
+  const b: number = 3;
+  const expected: number = 5;
+
+  // Act
+  const actual: number = divide(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+test("divide 15 by 0 is Infinity", () => {
+  // Arrange
+  const a: number = 15;
+  const b: number = 0;
+  const expected: number = Infinity;
+
+  // Act
+  const actual: number = divide(a, b);
 
   // Assert
   expect(actual).toBe(expected);
--- src/calculator.ts
+++ src/calculator.ts
@@ -5,3 +5,9 @@ export function add(summandA: number, summandB: number): number {
 export function subtract(minuend: number, subtrahend: number): number {
   return minuend - subtrahend;
 }
+export function multiply(factorA: number, factorB: number): number {
+  return factorA * factorB;
+}
+export function divide(dividend: number, divisor: number): number {
+  return dividend / divisor;
+}
