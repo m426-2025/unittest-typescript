7f6985b7f32b4038373f6f36cc773eb191774d68
Remo Albisser <remo_albisser@sluz.ch>
2025-03-21 11:21:30 +0100

    Aufgaben 1-6

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,13 +1,33 @@
 import { mean } from "./average";
 
-test("test mean of two numbers", () => {
+test("mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [2, 8];
+  const expected: number = 5;
 
   // Act
-  const actual: number = mean(numbers);
+  const actual = mean(numbers);
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test("mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const expected: number = 3;
+
+  // Act
+  const actual = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("mean of no numbers should throw an error", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Act & Assert
+  expect(() => mean(numbers)).toThrow("Liste darf nicht leer sein");
 });
