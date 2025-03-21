7b3b6f6eabf1c0b9e8abea236316a5412b057b50
Noah Langensee <noah.langensee@sluz.ch>
2025-03-21 14:58:33 +0100

    Aufgabe 6: Median Funktion hinzugefügt und mit Unit Tests überprüfen

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,4 +1,4 @@
-import { mean } from "./average";
+import { mean, median } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
@@ -33,4 +33,39 @@ test("test mean of no numbers", () => {
   expect(() => {
       mean(numbers);
     }).toThrow(new Error("Liste hat keine Elemente"));
+});
+
+test("test median of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+  // Act
+
+  // Assert
+  expect(() => {
+      median(numbers);
+    }).toThrow(new Error("Liste hat keine Elemente"));
+});
+
+test("test median of odd number of elements", () => {
+  // Arrange
+  const numbers: number[] = [3, 7, 6, 3, 5];
+  const expected: number = 5;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median of even number of elements", () => {
+  // Arrange
+  const numbers: number[] = [3, 6, 7, 4];
+  const expected: number = 5;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
 });
--- src/average.ts
+++ src/average.ts
@@ -9,3 +9,19 @@ export function mean(numbers: number[]): number {
   const mean: number = sum / numbers.length;
   return mean;
 }
+
+export function median(numbers: number[]): number {
+  let numberOfElements: number = numbers.length
+  let halfOfElements: number = Math.floor(numberOfElements/2)
+  if (numberOfElements==0){
+    throw new Error("Liste hat keine Elemente")
+  }
+  numbers.sort((n1,n2) => n1 - n2);
+  if (numberOfElements % 2 == 0){
+    return (numbers[halfOfElements] + numbers[halfOfElements - 1]) / 2
+  }
+  else{
+    return numbers[halfOfElements]
+  }
+  
+}
