2240564d921ffc1cb1494ae3b0913f8fb43b25fc
julianamschwand <julian_amschwand@sluz.ch>
2025-03-21 14:57:51 +0100

    4.1: Added division to calculate and a corresponding test

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract, multiply } from "./calculator";
+import { add, subtract, multiply, divide } from "./calculator";
 
 test("add three to one is four", () => {
   // Arrange
@@ -34,6 +34,19 @@ test("multiply three by four is twelve", () => {
 
   //Act
   const actual: number = multiply(a, b);
+
+  //Assert
+  expect(actual).toBe(expected);
+});
+
+test("divide fifteen by five is three", () => {
+  //Arrange
+  const a: number = 15;
+  const b: number = 5;
+  const expected: number = 3;
+
+  //Act
+  const actual: number = divide(a, b);
 
   //Assert
   expect(actual).toBe(expected);
