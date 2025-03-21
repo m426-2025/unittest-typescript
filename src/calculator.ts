091e4ee3814077302dc8c888288bd05f01551932
MARVIN HEGI <MARVIN_HEGI@sluz.ch>
2025-03-21 09:01:41 +0100

    Aufgabe 2.2 Manipulation rückgängig machen

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,5 @@
 export function add(summandA: number, summandB: number): number {
-  return summandA + summandB +1;
+  return summandA + summandB;
 }
 
 export function subtract(minuend: number, subtrahend: number): number {
