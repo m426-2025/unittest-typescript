994c24884162f3651ec406ab6b9151095978bf99
PHIL HÄGELI <phil.haegeli8@gmail.com>
2025-03-21 13:45:08 +0100

    Aufgabe 6 Median Tests + Code

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,4 +1,4 @@
-import { mean } from "./average";
+import { mean, median } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
@@ -30,4 +30,48 @@ test("test mean of no numbers", () => {
 
   // Assert
   expect(() => mean(numbers)).toThrow("Cannot calculate mean of an empty list");
+});
+
+test("test median for odd number of elements", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 6, 7, 8, 9, 10];
+  const expected: number = 7;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median for even number of elements", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 5, 6, 7, 8, 9, 10];
+  const expected: number = (6 + 7) / 2;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median for even number of elements", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 5, 6, 7, 8];
+  const expected: number = (5 + 6) / 2;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median for no elements", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Assert
+  expect(() => median(numbers)).toThrow("Cannot calculate median of an empty list");
 });
--- src/average.ts
+++ src/average.ts
@@ -9,3 +9,16 @@ export function mean(numbers: number[]): number {
   const mean: number = sum / numbers.length;
   return mean;
 }
+
+export function median(numbers: number[]): number {
+  if (numbers.length === 0) {
+    throw new Error("Cannot calculate median of an empty list");
+  }
+  numbers.sort((a, b) => a - b);
+  const mid = Math.floor(numbers.length / 2);
+  if (numbers.length % 2 === 0) {
+    return (numbers[mid - 1] + numbers[mid]) / 2;
+  } else {
+    return numbers[mid];
+  }
+}
