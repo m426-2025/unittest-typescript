06a0c2ec73cc6ff4cab0e8d37603daf348b94377
Arber Idrizaj <Arber_Idrizaj@sluz.ch>
2025-03-21 15:29:17 +0100

    Aufgabe 6.1

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract } from "./calculator";
+import { add, subtract, multiply, division } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
@@ -15,9 +15,9 @@ test("add three to two is five", () => {
 
 test("subtract four from six is two", () => {
   // Arrange
-  const a: number = 6;
+  const a: number = 5;
   const b: number = 4;
-  const expected: number = 2;
+  const expected: number = 1;
 
   // Act
   const actual: number = subtract(a, b);
@@ -25,3 +25,34 @@ test("subtract four from six is two", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("multiply 3 times 4 is 12", () => {
+const a: number = 3;
+const b: number = 4;
+const expected: number = 12;
+
+
+const actual: number = multiply(a,b);
+
+expect(actual).toBe(expected);
+
+})
+
+test("divide 5 from 2 is 2.5", () => {
+  const a: number = 5;
+  const b: number = 2;
+  const expected: number = 2.5;
+
+  const actual: number = division(a,b);
+
+  expect(actual).toBe(expected);
+
+})
+
+
+test("divide 0 from 5 is infinet", () => {
+  const a: number = 5;
+  const b: number = 0;
+  expect(() => division(a, b)).toThrow("Division durch 0 nicht erlaubt");
+
+})
