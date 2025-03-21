78ab435b6a771daedf4dd2e1caba3b9ffe430ad5
aaron-swiss <aaron.ettlin07@gmail.com>
2025-03-21 13:09:45 +0100

    Aufgabe 3.2: multiply implementiert

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,10 +1,10 @@
-import { add, subtract } from "./calculator";
+import { add, subtract, multiply } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
-  const a: number = 2;
+  const a: number = 3;
   const b: number = 3;
-  const expected: number = 5;
+  const expected: number = 6;
 
   // Act
   const actual = add(a, b);
@@ -21,6 +21,19 @@ test("subtract four from six is two", () => {
 
   // Act
   const actual: number = subtract(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("multiply five by three is fifteen", () => {
+  // Arrange
+  const a: number = 5;
+  const b: number = 3;
+  const expected: number = 15;
+
+  // Act
+  const actual: number = multiply(a, b);
 
   // Assert
   expect(actual).toBe(expected);
