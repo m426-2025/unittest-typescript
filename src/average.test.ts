81b8b73b84e25618cddb4e978ce87512abe1e71e
Artur Ferreira <artur.ferreira@ckw.ch>
2025-03-21 09:10:03 +0100

    aufgabe 5.2: test geschrieben

--- src/average.test.ts
+++ src/average.test.ts
@@ -11,3 +11,14 @@ test("test mean of two numbers", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const expected: number = 3;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
