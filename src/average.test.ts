4257dee4c8b70ef88229057547ae205b84170246
Nils Steiner <Nils_Steiner@sluz.ch>
2025-03-21 13:48:23 +0100

    Aufgabe 5: mean()-Funktion angepasst das wenn eine Liste die nicht drin hat einen Fehler geworfen wird.

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,12 +2,36 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [3, 6];
+  const expected: number = 4.5;
 
   // Act
   const actual: number = mean(numbers);
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [1, 3, 5, 7, 9];
+  const expected: number = 5;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test mean of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+  const expected: number = 0;
+
+  // Act
+  const act = () => mean(numbers);
+
+  // Assert
+  expect(act).toThrowError("List cant be processed");
 });
