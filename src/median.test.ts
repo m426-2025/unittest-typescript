e447ebdaff8c88bedb85e5fc895374e587f3a238
HeiligerG <gggigi111@gmail.com>
2025-03-24 17:19:49 +0100

    Aufgabe 6.3

--- src/median.test.ts
+++ src/median.test.ts
@@ -2,16 +2,16 @@ import { median } from "./median";
 
 test("test median for odd number of elements", () => {
     // Arrange
-    const numbers = [1, 2, 6, 7, 8];
-    const expected = 6;
+    const numbers = [1, 2, 6, 7, 8, 9, 10];
+    const expected = 7;
     // Act
     const actual = median(numbers);
     // Assert
     expect(actual).toBe(expected);
-});
+}); 
 
 test("median for even number of elements", () => {
-    const numbers = [1, 2, 5, 6, 7, 8];
+    const numbers = [1, 2, 4, 5, 6, 7, 8, 9];
     const expected = (5 + 6) / 2;
     const actual = median(numbers);
     expect(actual).toBe(expected);
