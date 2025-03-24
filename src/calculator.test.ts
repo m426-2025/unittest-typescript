77bd8362c757b25c3081095ed82019e4f0da6006
HeiligerG <gggigi111@gmail.com>
2025-03-24 16:46:46 +0100

    Aufgabe 2.3

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -2,9 +2,9 @@ import { add, subtract } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
-  const a: number = 2;
+  const a: number = 3;
   const b: number = 3;
-  const expected: number = 5;
+  const expected: number = 6;
 
   // Act
   const actual = add(a, b);
@@ -15,9 +15,9 @@ test("add three to two is five", () => {
 
 test("subtract four from six is two", () => {
   // Arrange
-  const a: number = 6;
+  const a: number = 5;
   const b: number = 4;
-  const expected: number = 2;
+  const expected: number = 1;
 
   // Act
   const actual: number = subtract(a, b);
