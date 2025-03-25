e8ad471b644fcd5ae660b2a5623826e676a10872
Janik Stucki <Janik_Stucki@sluz.ch>
2025-03-25 13:45:49 +0100

    Median Funktion überarbeitet und getestet mit den Aufgaben 6.2 und 6.3.2.

--- src/median.test.ts
+++ src/median.test.ts
@@ -14,7 +14,7 @@ test("test median for odd number of elements", () => {
 
 test("test median for even number of elements", () => {
   // Arrange
-  const numbers: number[] = [1, 2, 3, 4];
+  const numbers: number[] = [4, 2, 1, 3];
   const expected: number = 2.5;
 
   // Act
--- src/median.ts
+++ src/median.ts
@@ -1,5 +1,10 @@
 export function median(numbers: number[]): number {
+    const sorted = numbers.sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
 
-    const median =  numbers.sort((a, b) => a - b);
-    return median
+    if (sorted.length % 2 === 0) {
+        return (sorted[middle - 1] + sorted[middle]) / 2;
+    } else {
+        return sorted[middle];
+    }
 }
