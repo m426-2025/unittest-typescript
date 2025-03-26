7735c4a76ad1f02f29a2b5911e8d514e6bc331df
Gregory ruoss <gregory_ruoss@sluz.ch>
2025-03-26 13:28:25 +0100

    Aufgaben 4.1, 4.2, 4.3 fertig mit dem devide through 0 function and error response

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract, multiply } from "./calculator";
+import { add, subtract, multiply, divide } from "./calculator";
 
 test("add four to three is seven", () => {
   // Arrange
@@ -37,4 +37,26 @@ test("mutliply two by three is six", () => {
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test("divide ten by two is five", () => {
+  // Arrange
+  const a: number = 10;
+  const b: number = 2;
+  const expected: number = 5.0;
+
+  // Act
+  const actual: number = divide(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("divide ten by zero is Infinity", () => {
+  // Arrange
+  const a: number = 10;
+  const b: number = 0;
+
+  // Assert
+  expect(() => divide(a, b)).toThrow("Division by zero is not allowed.");
 });
