0f0943d62acb5bed268ecb70ed6fdc9a2c828da4
Gregory ruoss <gregory_ruoss@sluz.ch>
2025-03-26 13:55:27 +0100

    Aufgaben 6.1, 6.2, 6.3, 6.4 finished made median for no,odd,even elements and added a error for no elements made the function sort for random numbers and tested all of them

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,4 +1,4 @@
-import { mean } from "./average";
+import { mean, median } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
@@ -28,9 +28,54 @@ test("test mean of five numbers", () => {
   
     //Arrange
     const numbers: number[] = [];
-    const expected: number = 0;
 
     //Assert
     expect(() => mean(numbers)).toThrow("The numbers can't be zero.");
 
   })
+
+test("test median of odd number of elements", ()=>{
+
+  //Arrange
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const expected: number = 3;
+
+  //Act
+  const actual: number = median(numbers);
+
+  //Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median of even number of elements", ()=>{
+  
+    //Arrange
+    const numbers: number[] = [1, 2, 3, 4];
+    const expected: number = 2.5;
+  
+    //Act
+    const actual: number = median(numbers);
+  
+    //Assert
+    expect(actual).toBe(expected);
+});
+
+test("test median of zero number of elements", () =>{
+  //Arrange
+  const numbers: number[] = [];
+
+  //Assert
+  expect(() => median(numbers)).toThrow("The numbers can't be zero.");
+});
+
+test("test median of random numbers", () =>{
+  //Arrange
+  const numbers: number[] = [4, 2, 3, 1, 5];
+  const expected: number = 3;
+
+  //Act
+  const actual: number = median(numbers);
+
+  //Assert
+  expect(actual).toBe(expected);
+});
--- src/average.ts
+++ src/average.ts
@@ -9,3 +9,15 @@ export function mean(numbers: number[]): number {
   const mean: number = sum / numbers.length;
   return mean;
 }
+
+export function median(numbers: number[]): number{
+  if (numbers.length === 0){
+    throw new Error("The numbers can't be zero.");
+  }
+  numbers.sort((a, b) => a - b);
+  const middle: number = Math.floor(numbers.length / 2);
+  if (numbers.length % 2 === 0){
+    return mean([numbers[middle - 1], numbers[middle]]);
+  }
+  return numbers[middle];
+}
