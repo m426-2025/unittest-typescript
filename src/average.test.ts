55a3ce6236758684d5e180f31eac96a64f084168
Gregory ruoss <gregory_ruoss@sluz.ch>
2025-03-26 13:41:22 +0100

    Aufgabe 5.1, 5.2, 5.3 finished swiched numbers and made them work added a new test for five numbers and added a error for when there are 0 numbers

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,8 +2,8 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [2, 8];
+  const expected: number = 5;
 
   // Act
   const actual: number = mean(numbers);
@@ -11,3 +11,26 @@ test("test mean of two numbers", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const expected: number = 3;
+
+  //Act 
+  const actual: number = mean(numbers);
+
+  //Assert
+  expect(actual).toBe(expected);
+  });
+
+  test("test mean of zero numbers", () =>{
+  
+    //Arrange
+    const numbers: number[] = [];
+    const expected: number = 0;
+
+    //Assert
+    expect(() => mean(numbers)).toThrow("The numbers can't be zero.");
+
+  })
