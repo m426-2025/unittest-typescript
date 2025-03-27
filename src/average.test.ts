8531212e0bb5f003520d8b8a87f5c616b082ad4f
Eneas Infanger <eneas_infanger@sluz.ch>
2025-03-27 08:03:12 +0100

    Aufgabe 6: Median hinzugefügt
    - Die `median` Berechnungsfunktion und Tests dazu hinzugefügt

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,4 +1,4 @@
-import { mean } from "./average";
+import {mean, median} from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
@@ -31,3 +31,36 @@ test("test mean of no numbers", () => {
   // Act / Assert
   expect(() => mean(numbers)).toThrow();
 });
+
+test("test median for odd number of elements", () => {
+  // Arrange
+  const numbers: number[] = [4, 6, 13, 1, 20, 32, 21]; // sorted: [1, 4, 6, 13, 21, 32]
+  const expected: number = 13;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median for even number of elements", () => {
+  // Arrange
+  const numbers: number[] = [4, 6, 13, 2, 20, 18, 32, 21]; // sorted: [2, 4, 6, 13, 18, 20, 21, 32]
+  const expected: number = 15.5;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("test median for no elements", () => {
+  // Arrange
+  const numbers: number[] = [];
+
+  // Act / Assert
+  expect(() => median(numbers)).toThrow();
+});
+
--- src/average.ts
+++ src/average.ts
@@ -9,3 +9,16 @@ export function mean(numbers: number[]): number {
   const mean: number = sum / numbers.length;
   return mean;
 }
+
+export function median(numbers: number[]): number {
+  if (numbers.length === 0) {
+    throw new Error("no numbers provided");
+  }
+  numbers = numbers.toSorted((a, b) => a - b);
+  const mid = Math.floor(numbers.length / 2);
+  if (numbers.length % 2 !== 0) {
+    return numbers[mid];
+  } else {
+    return (numbers[mid - 1] + numbers[mid]) / 2;
+  }
+}
