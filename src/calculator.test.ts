8ff0c248f2e1eb2039eaa4c2c225f7f55aefd930
nando brun <nando_brun@sluz.ch>
2025-03-27 13:55:11 +0100

    unit-test Aufgaben.

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,10 +1,10 @@
-import { add, subtract } from "./calculator";
+import { add, subtract, multiply, divide } from "./calculator";
 
-test("add three to two is five", () => {
+test("add three to four is seven", () => {
   // Arrange
-  const a: number = 2;
-  const b: number = 3;
-  const expected: number = 5;
+  const a: number = 3;
+  const b: number = 4;
+  const expected: number = 7;
 
   // Act
   const actual = add(a, b);
@@ -13,14 +13,53 @@ test("add three to two is five", () => {
   expect(actual).toBe(expected);
 });
 
-test("subtract four from six is two", () => {
+test("subtract two from five is three", () => {  
   // Arrange
-  const a: number = 6;
-  const b: number = 4;
-  const expected: number = 2;
+  const a: number = 5;
+  const b: number = 2;
+  const expected: number = 3;
 
   // Act
   const actual: number = subtract(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("multiply four to three is twelve", () => { 
+  // Arrange
+  const a: number = 4;
+  const b: number = 3;
+  const expected: number = 12;
+
+  // Act
+  const actual: number = multiply(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("divide three from twelve is four", () => { 
+  // Arrange
+  const a: number = 12;
+  const b: number = 3;
+  const expected: number = 4;
+
+  // Act
+  const actual: number = divide(a, b);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("divide zero from twelve is Infinity", () => {
+  // Arrange
+  const a: number = 12;
+  const b: number = 0;
+  const expected: number = Infinity;
+
+  // Act
+  const actual: number = divide(a, b);
 
   // Assert
   expect(actual).toBe(expected);
