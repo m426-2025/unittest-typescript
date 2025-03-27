eb5e0475adcdf2e3bd6c7515784ee8dd15d49221
Yusuf Er <yusuf_er@sluz.ch>
2025-03-27 17:33:14 +0100

    Aufgabe 5.1: Arithmetisches Mittel für zwei Zahlen und Fehlerbehandlung für leere Liste hinzugefügt

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,13 +1,17 @@
 import { mean } from "./average";
 
-test("test mean of two numbers", () => {
+test("test mean of five numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const expected: number = 3;
 
   // Act
   const actual: number = mean(numbers);
 
   // Assert
   expect(actual).toBe(expected);
+});
+test('test mean of no numbers', () => {
+  const numbers: number[] = [];
+  expect(() => mean(numbers)).toThrow('Cannot calculate mean of an empty array');
 });
