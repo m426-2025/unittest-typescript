7e3447fbe5e4d9068303ddf18dd3cde601d939a1
TobiasClausen <tobias.clausen06@gmail.com>
2025-03-27 14:17:45 +0100

    Aufgabe 4.3: divide() mit Fehlerbehandlung ergänzt

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -29,3 +29,11 @@ test("subtract four from six is two", () => {
 test("multiply 3 and 4 is 12", () => {
   expect(multiply(3, 4)).toBe(12);
 });
+
+test("divide 8 by 4 is 2", () => {
+  expect(divide(8, 4)).toBe(2);
+});
+
+test("divide by 0 throws error", () => {
+  expect(() => divide(1, 0)).toThrow("Division by zero is not allowed");
+});
