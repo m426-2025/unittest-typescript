1a6b06583b31a41d9050a75c805f898ead84ac8e
Timo Fuchs <Timo_Fuchs@sluz.ch>
2025-03-28 10:24:00 +0100

    Aufgaben 2-6 nachcommiten

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,10 +1,11 @@
-import { add, subtract } from "./calculator";
+import { mean, median } from "./average";
+import { add, divide, multiply, subtract } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
   const a: number = 2;
-  const b: number = 3;
-  const expected: number = 5;
+  const b: number = 4;
+  const expected: number = 6;
 
   // Act
   const actual = add(a, b);
@@ -24,4 +25,67 @@ test("subtract four from six is two", () => {
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test("multiply three by two is six", () => {
+  // Arrange
+  const a: number = 3;
+  const b: number = 2;
+  const expected: number = 6;
+
+  // Act
+  const actual = multiply(a, b); 
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("divide six by two is three", () => {
+  // Arrange
+  const a: number = 6;
+  const b: number = 2;
+  const expected: number = 3;
+
+  // Act
+  const actual = divide(a, b); 
+
+// Assert
+  expect(actual).toBe(expected);
+});
+
+test("divide by zero throws error", () => {
+  const a: number = 6;
+  const b: number = 0;
+  expect(() => divide(a, b)).toThrowError("Division by zero is not allowed");
+});
+
+test("test mean of five numbers", () => {
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const expected: number = 3;
+  const actual: number = mean(numbers);
+  expect(actual).toBe(expected);
+});
+
+test("test mean of no numbers", () => {
+  const numbers: number[] = [];
+  expect(() => mean(numbers)).toThrowError("Cannot calculate mean of an empty list");
+});
+
+test("test median for odd number of elements", () => {
+  const numbers: number[] = [1, 3, 2];
+  const expected: number = 2;
+  const actual: number = median(numbers);
+  expect(actual).toBe(expected);
+});
+
+test("test median for even number of elements", () => {
+  const numbers: number[] = [1, 2, 3, 4];
+  const expected: number = 2.5;
+  const actual: number = median(numbers);
+  expect(actual).toBe(expected);
+});
+
+test("test median for no elements", () => {
+  const numbers: number[] = [];
+  expect(() => median(numbers)).toThrowError("Cannot calculate median of an empty list");
 });
