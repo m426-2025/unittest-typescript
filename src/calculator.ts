afe32bba179d9627abd5ca749cfc576c3e132543
Michael <Michael_Hurschler@sluz.ch>
2025-03-28 13:12:31 +0100

    Aufgabe 2

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,7 +1,7 @@
-export function add(summandA: number, summandB: number): number {
-  return summandA + summandB;
+export function add(a: number, b: number): number {
+  return a + b;
 }
 
-export function subtract(minuend: number, subtrahend: number): number {
-  return minuend - subtrahend;
+export function subtract(a: number, b: number): number {
+  return a - b;
 }
