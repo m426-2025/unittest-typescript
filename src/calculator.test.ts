afbeb70cc67b792459856d247cb9112ddd47b56f
Michael <Michael_Hurschler@sluz.ch>
2025-03-28 13:15:02 +0100

    Aufgabe 4

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract, multiply } from "./calculator";
+import { add, subtract, multiply, divide } from "./calculator";
 
 test("add three to two is five", () => {
   const a: number = 2;
@@ -12,7 +12,7 @@ test("subtract four from six is two", () => {
   const a: number = 6;
   const b: number = 4;
   const expected: number = 2;
-  const actual: number = subtract(a, b);
+  const actual = subtract(a, b);
   expect(actual).toBe(expected);
 });
 
@@ -22,4 +22,16 @@ test("multiply three with four is twelve", () => {
   const expected: number = 12;
   const actual = multiply(a, b);
   expect(actual).toBe(expected);
+});
+
+test("divide eight by two is four", () => {
+  const a: number = 8;
+  const b: number = 2;
+  const expected: number = 4;
+  const actual = divide(a, b);
+  expect(actual).toBe(expected);
+});
+
+test("divide by zero throws error", () => {
+  expect(() => divide(5, 0)).toThrow("Cannot divide by zero");
 });
