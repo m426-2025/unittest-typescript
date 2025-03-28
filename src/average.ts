9918a88c9cb32b5aacd021653020b1d98aaee68b
Nando Schürmann <nando.schuermann@getabstract.com>
2025-03-28 10:44:54 +0100

    Add median function and improve tests for mean and calculator
    Added a new `median` function to calculate the median of a number array, along with improved error handling for empty arrays in the `mean` function. Expanded test coverage for `mean`, `median`, and new calculator operations (`multiply` and `divide`), including edge cases like division by zero.

--- src/average.ts
+++ src/average.ts
@@ -1,8 +1,23 @@
 export function mean(numbers: number[]): number {
-  let sum: number = 0;
-  for (let number of numbers) {
-    sum += number;
+  if (numbers.length !== 0) {
+    let sum: number = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum / numbers.length;
   }
-  const mean: number = sum / numbers.length;
-  return mean;
+  throw new Error("Empty Array")
+}
+
+export function median(numbers: number[]): number {
+  if (numbers.length !== 0) {
+    const sortedNumbers = [...numbers].sort((a, b) => a - b)
+    const middle = Math.floor(sortedNumbers.length / 2);
+    if (sortedNumbers.length % 2 === 0) {
+      return (sortedNumbers[middle - 1] + sortedNumbers[middle]) / 2;
+    } else {
+      return sortedNumbers[middle];
+    }
+  }
+  throw new Error("No Elements");
 }
