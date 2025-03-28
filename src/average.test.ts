202325415b46de6edc8593cd2db7965cb7750c01
TheGamingSimon <simon.pichler@bluewin.ch>
2025-03-28 11:12:35 +0100

    20250328 Commit 3 Aufgabe 5

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,12 +2,25 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [4, 5];
+  const expected: number = 4.5;
 
   // Act
   const actual: number = mean(numbers);
 
   // Assert
   expect(actual).toBe(expected);
+});
+
+test('mean of five numbers', () => {
+  const numbers = [1, 2, 3, 4, 5];
+  const expected = 3;
+
+  const result = mean(numbers);
+
+  expect(result).toBe(expected);
+});
+
+test('mean of no numbers throws error', () => {
+  expect(() => mean([])).toThrow('Cannot calculate mean of empty list');
 });
--- src/average.ts
+++ src/average.ts
@@ -4,5 +4,8 @@ export function mean(numbers: number[]): number {
     sum += number;
   }
   const mean: number = sum / numbers.length;
+  if (numbers.length === 0) {
+    throw new Error('Cannot calculate mean of empty list');
+  }
   return mean;
 }
