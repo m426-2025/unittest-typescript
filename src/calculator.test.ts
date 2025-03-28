94b537fcc0e2b51203e70753e4dda42d45c0630a
Simon Pichler <simon_pichler1@sluz.ch>
2025-03-28 10:26:47 +0100

    20250328 Commit 1

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,10 +1,10 @@
-import { add, subtract } from "./calculator";
+import { add, division, multiply, subtract } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
-  const a: number = 2;
+  const a: number = 1;
   const b: number = 3;
-  const expected: number = 5;
+  const expected: number = 4;
 
   // Act
   const actual = add(a, b);
@@ -25,3 +25,42 @@ test("subtract four from six is two", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("multiply three times 4 is twelve", () => {
+// Arrange
+const a: number = 3;
+const b: number = 4;
+const expected: number = 12;
+
+// Act
+const actual: number = multiply(a, b);
+
+// Assert
+expect(actual).toBe(expected);
+})
+
+test("divide fifty-six by seven is eight", () => {
+  // Arrange
+  const a: number = 56;
+  const b: number = 7;
+  const expected: number = 8;
+  
+  // Act
+  const actual: number = division(a, b);
+  
+  // Assert
+  expect(actual).toBe(expected);
+})
+
+test("divide fifty-six by zero is eight", () => {
+  // Arrange
+  const a: number = 56;
+  const b: number = 0;
+  const expected: number = Infinity;
+  
+  // Act
+  const actual: number = division(a, b);
+  
+  // Assert
+  expect(actual).toBe(expected);
+})
--- src/calculator.ts
+++ src/calculator.ts
@@ -5,3 +5,11 @@ export function add(summandA: number, summandB: number): number {
 export function subtract(minuend: number, subtrahend: number): number {
   return minuend - subtrahend;
 }
+
+export function multiply(faktorA: number, faktorB: number): number{
+  return faktorA * faktorB
+}
+
+export function division(divisor: number, dividend: number): number{
+  return divisor / dividend
+}
